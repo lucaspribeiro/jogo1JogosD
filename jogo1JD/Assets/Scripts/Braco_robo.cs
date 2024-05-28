855d00840b47124866d941fdce8f1a3bebba54a6
luisEscm <luys.martins27@gmail.com>
2024-05-28 03:58:02 -0300

    commit
    é isso

--- jogo1JD/Assets/Scripts/Braco_robo.cs
+++ jogo1JD/Assets/Scripts/Braco_robo.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Braco_robo : MonoBehaviour
 {
+    [SerializeField] private Transform braco;
+    [SerializeField] private GameObject mao;
+    [SerializeField] public Transform shootPoint;
+    private Camera mainCam;
+    private Vector3 mousePos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        braco = GetComponent<Transform>();
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direcao = mousePos - braco.position;
+        float rotZ = Mathf.Atan2(direcao.y, direcao.x) * Mathf.Rad2Deg;
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject maoInt = Instantiate(mao, shootPoint.position, Quaternion.identity);
+            maoInt.GetComponent<Transform>().rotation = Quaternion.Euler(0, 0, rotZ);
+            maoInt.GetComponent<MaoBot>().GetComponent<LineRenderer>().positionCount = 2;
+            maoInt.GetComponent<MaoBot>().position = direcao;
+            maoInt.GetComponent<MaoBot>().braco = shootPoint;
+        }
+        else
+        {
+            braco.rotation = Quaternion.Euler(0, 0, rotZ-90);
+        }
+
+
     }
 }
---
+++ jogo1JD/Assets/Scripts/MaoBot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaoBot : MonoBehaviour
+{
+    public Vector3 position;
+    public Transform braco;
+    public LineRenderer linha;
+    // Start is called before the first frame update
+    void Start()
+    {
+        linha = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(position.normalized * Time.deltaTime * 25);
+        if (linha.positionCount == 2)
+        {
+            GetComponent<LineRenderer>().SetPosition(0, transform.position);
+            GetComponent<LineRenderer>().SetPosition(1, braco.position);
+        }
+    }
+}
